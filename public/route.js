085c92e360caee553301d1dc79e615a49d9e8da4
Kritsada Chan-inn <bankth99@gmail.com>
2025-06-18 20:32:40 +0700

    add delete and update

--- public/route.js
+++ public/route.js
@@ -3,6 +3,8 @@ const path = require(`path`);
 
 const router = express.Router();
 
+const fs = require(`fs`);
+
 const { readdirSync } = require("fs");
 const GalleryItem = require("../model/galleryItem");
 const { resolve6 } = require("dns");
@@ -38,33 +40,54 @@ router.get(`/gallery`, (request, response) => {
     
 })
 
+router.get('/favicon.ico', (req, res) => res.status(204).end());
+
 router.get(`/addGalleryItem`, (request, response) => {
     response.render(`addGalleryItem.ejs`);
 })
 
-router.get(`/:id`, (request, response) => {
-    console.log(request.params.id);
+router.get(`/galleryPage-:id`, (request, response) => {
 
     GalleryItem.findOne({_id:request.params.id}).exec()
         .then(document => {
-            console.log(document);
             response.render(`galleryPage.ejs`,{gallery_item:document});
         })
         .catch((err) => {
             console.log(err);
         })
-
-    
 })
 
+router.get(`/delete/:id`,(request, response) => {
+    GalleryItem.findOne({_id:request.params.id}).exec()
+    .then(document => {
+        const imageName = document.image;
+        const imagePath = path.join(__dirname, `galleryImages`, imageName);
+        console.log(imagePath);
+        fs.unlink(imagePath, err => {
+            if(err) console.log(err);
+        })
+    })
+    .catch(err => {
+        console.log(err);
+    })
 
+    GalleryItem.findByIdAndDelete(request.params.id, {useFindAndModify:false}).exec()
+        .then(() =>{
+            console.log(request.params.id);
+            
+            response.redirect(`/gallery`);
+        })
+        .catch((err) => {
+            console.log(err);
+        })
 
+})
 
 router.post(`/sendingAddItem`, upload.single(`image`), (request, response) => {
     
     
-    console.log(request.file);
-    console.log(`------------------`);
+    // console.log(request.file);
+    // console.log(`------------------`);
     let formattedDate = request.body.datetime.replace('T', ' ');
 
 
@@ -75,8 +98,6 @@ router.post(`/sendingAddItem`, upload.single(`image`), (request, response) => {
         content: request.body.content
     })
 
-    console.log(document);
-
     document.save()
         .then(() => {
             response.redirect(`/gallery`)
@@ -86,6 +107,35 @@ router.post(`/sendingAddItem`, upload.single(`image`), (request, response) => {
             response.status(500).send('Error saving document');
         });
 
+})
+
+router.post(`/edit`, (request, response) => {
+    GalleryItem.findOne({_id:request.body.edit_id}).exec()
+        .then(document => {
+            let formattedDate = document.date.replace(' ', 'T');
+            document.date = formattedDate;
+            response.render(`editItem.ejs`,{gallery_item:document});
+        })
+        .catch(err => {
+            console.log(err);
+        })
+})
+
+router.post(`/updateItem`,(request, response) => {
+    
+    let formattedDate = request.body.datetime.replace('T', ' ');
+    const data = {
+        datetime: formattedDate,
+        topic: request.body.topic,
+        content: request.body.content
+    };
+    GalleryItem.findByIdAndUpdate(request.body._id, data).exec()
+        .then(() => {
+            response.redirect(`/gallery`);
+        })
+        .catch((err) => {
+            console.log(err);
+        })
 })
 
 module.exports = router;
